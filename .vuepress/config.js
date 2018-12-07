abfabc691490fd561c019393c2ee4f373ecd6493
MeFelixWang <867087554@qq.com>
2018-12-07 11:52:09 +0800

    Init project

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    base:'/Flutter-Manual-zh/',
+    title: 'Flutter 中文手册',
+    description: 'Flutter 中文手册',
+    themeConfig: {
+        nav: [
+            {text: '首页', link: '/'}
+        ],
+        sidebar: [
+            ['/Widgets', '组件'],
+            ['/InteractionModels', '交互模型'],
+        ],
+        sidebarDepth: 1,
+        lastUpdated: '最后更新于：'
+    }
+};
---
+++ InteractionModels.md
@@ -0,0 +1,5 @@
+## Draggable
+
+可以在[DragTarget](#DragTarget)中拖动的组件。
+
+## DragTarget
